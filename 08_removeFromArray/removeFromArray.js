acda864eddfb67bac91814e051e26ce2a71e85bc
Aviipatell <avipatel.dev@gmail.com>
2025-10-29 15:41:22 -0400

    Complete 08-removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(item => {
+        if (args.includes(item)) return false;
+        return true;
+    });
 };
 
 // Do not edit below this line
