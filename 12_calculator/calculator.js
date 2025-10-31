54f5793a4a7931700b19ab4756396e71b8a093b1
Aviipatell <avipatel.dev@gmail.com>
2025-10-31 18:31:01 -0400

    Complete 12-calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((accumulator, current) => {
+    accumulator+= current;
+    return accumulator;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((accumulator, current) => {
+    accumulator*= current;
+    return accumulator;
+  });
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let value = 1;
+  for (let i = 0; i < exponent; ++i) {
+    value*= base;
+  }
+  return value;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let value = 1;
+  for (let i = 1; i <= num; ++i) {
+    value*= i;
+  }
+  return value;
 };
 
 // Do not edit below this line
