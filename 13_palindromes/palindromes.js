2c62c4327ad9056ef3e34405ce2ff12572ed6616
Aviipatell <avipatel.dev@gmail.com>
2025-11-01 16:50:17 -0400

    Complete 13-palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let filteredStr = str.toLowerCase().split('').filter((item) => {
+        return /[a-zA-Z0-9]/.test(item);
+    });
+    // Note, Array.reverse() is a mutable method so it would cause equality check to always return true
+    let reverseStr = filteredStr.toReversed().join("");
+    filteredStr = filteredStr.join("");
+    
+    return (filteredStr === reverseStr);
 };
 
 // Do not edit below this line
