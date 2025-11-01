4a9105e8ddee5e4aa94a2743a6001a8aaa61233a
Aviipatell <avipatel.dev@gmail.com>
2025-11-01 17:05:51 -0400

    Complete 16-findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    // Option 1: loop through array with a oldest obj declared outside. Calculate age per element, if older than oldest obj, set current to be new oldest
 
+    // Option 2: use reduce to collapse array into one entity which is the oldest
+
+    // Let accumulator default val be first element, leave option initial value be empty
+    return peopleArr.reduce((accumulator, current) => {
+        if (getAge(current) > getAge(accumulator)) {
+            accumulator = current;
+            return accumulator;
+        } else {
+            return accumulator;
+        }
+    });
 };
+
+const getAge = function(person) {
+    let age;
+    if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = new Date().getFullYear() - person.yearOfBirth;
+    }
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
