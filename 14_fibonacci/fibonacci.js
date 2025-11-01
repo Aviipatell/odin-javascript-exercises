ffab2ac33875c29922d76c028beaad991b1528cc
Aviipatell <avipatel.dev@gmail.com>
2025-11-01 16:55:12 -0400

    Complete 14-fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let intNum = +num;
+    if (intNum < 0) return "OOPS";
+    if (intNum == 0) {
+        return 0;
+    } else if (intNum == 1) {
+        return 1;
+    } else {
+        return fibonacci(intNum-1) + fibonacci(intNum-2)
+    }
 };
 
 // Do not edit below this line
